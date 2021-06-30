233fe6e92b47a31cece5f6ec19c7f2066de57fd5
mehedihasankhairul <mhkhairul78@gmail.com>
2021-06-30 06:23:51 +0600

    port update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 app.use(cors())
 app.use(express.json())
 
-const port = 5000
+const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1eg3a.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
